f165ff5b00fac8e3759267d5e36b111f8eebe8e4
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-12-31 00:02:03 +0530

    request file added

---
+++ src/request.js
@@ -0,0 +1,6 @@
+const API_KEY = "d05f0d9230141b9170188da04170c454";
+const requests = {
+  TrendingMoviesOfTheWeek:
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=" + API_KEY,
+};
+export default requests;
