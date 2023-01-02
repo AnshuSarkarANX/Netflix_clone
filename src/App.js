8d000697ae915bcd303ee1b133f322f17bc3a627
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-01-02 23:48:52 +0530

    react imported and row component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+/* eslint-disable react/jsx-no-undef */
+import React from "react";
+import Row from "./Row";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>netflix-clone website</h1>
+      {<Row title="Originals" />}
+      {<Row title="Trending Movies" />}
     </div>
   );
 }
