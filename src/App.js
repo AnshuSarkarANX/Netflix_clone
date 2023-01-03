298034d3afe1d7c1dcdd0c84266e98fcf0aaaa6d
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-01-03 23:54:59 +0530

    url added in row section from request file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import React from "react";
 import Row from "./Row";
 import "./App.css";
+import request from "./request";
 
 function App() {
   return (
     <div className="App">
       <h1>netflix-clone website</h1>
-      {<Row title="Originals" />}
-      {<Row title="Trending Movies" />}
+      {<Row title="Originals" fetchUrl={request.NetflixOriginals} />}
+      {<Row title="Trending Movies" fetchUrl={request.Trending} />}
     </div>
   );
 }
