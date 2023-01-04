d1f3ef31db4230eb43e7ff2710f053a00bc88c00
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-01-04 22:51:15 +0530

    useEffect added with async function from fetvhing data from the api

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+    }
+    fetchData();
+  }, []);
   return (
     <div>
       <h2>{title}</h2>
