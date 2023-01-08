26aeb8ed78621469c777ac64a2e20b7daa842e92
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-01-08 23:55:49 +0530

    row.css file imported

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 const img_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
