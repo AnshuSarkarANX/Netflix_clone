e6e56011e2388ab0aa0bf0e735d564436553088b
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-02-06 00:39:46 +0530

    Update: changed for differnet thumnails

--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 const img_url = "https://image.tmdb.org/t/p/original/";
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -19,8 +19,11 @@ function Row({ title, fetchUrl }) {
       <div className="row_posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row_image"
-            src={`${img_url}${movie.poster_path}`}
+            src={`${img_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.original_title}
           ></img>
         ))}
