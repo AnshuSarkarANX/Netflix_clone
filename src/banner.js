3ea0caa91f0a67cacdb292de9004f33799d9a960
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-02-07 22:30:45 +0530

    new file created

---
+++ src/banner.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+
+function Banner() {
+    const [movie,setMovie]= useState([]);
+    useEffect(()=>{
+
+    })
+
+  return (
+    <div></div>
+  )
+}
+
+export default Banner
