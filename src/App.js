2087bcf754cebdc3decc4a1298b7207dce47e31e
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-02-09 22:15:35 +0530

    feat: banner added ata the top

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import Row from "./Row";
 import "./App.css";
 import request from "./request";
+import Banner from "./banner";
 
 function App() {
   return (
-    <div className="App">
-      <h1>NETFLIX</h1>
-      <Row title="Originals" fetchUrl={request.NetflixOriginals} isLargeRow />
-      <Row title="Trending Movies" fetchUrl={request.Trending} />
-      <Row title="TopRated Movies" fetchUrl={request.TopRated} />
-      <Row title=".Action Movies" fetchUrl={request.ActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={request.ComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={request.HorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={request.RomanceMovies} />
-      <Row title="Documantaries" fetchUrl={request.Documantaries} />
-    </div>
+    <>
+      <Banner />
+      <div className="App">
+        <h1>NETFLIX</h1>
+        <Row title="Originals" fetchUrl={request.NetflixOriginals} isLargeRow />
+        <Row title="Trending Movies" fetchUrl={request.Trending} />
+        <Row title="TopRated Movies" fetchUrl={request.TopRated} />
+        <Row title=".Action Movies" fetchUrl={request.ActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={request.ComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={request.HorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={request.RomanceMovies} />
+        <Row title="Documantaries" fetchUrl={request.Documantaries} />
+      </div>
+    </>
   );
 }
 
