50a5213204b7db941f5148b2e8f4f92e74cec090
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-02-09 22:15:16 +0530

    feat: show title,ov erview,play buttons

--- src/banner.js
+++ src/banner.js
@@ -1,14 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./request";
+import "./banner.css";
+const img_url = "https://image.tmdb.org/t/p/original/";
 
 function Banner() {
-    const [movie,setMovie]= useState([]);
-    useEffect(()=>{
-
-    })
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.NetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * (request.data.results.length - 1))
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
 
   return (
-    <div></div>
-  )
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("${img_url}${movie.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="BannerComponents">
+        <h1>{movie.title || movie.name || movie.original_name}</h1>
+        <div>
+          <button className="ButtonBanner">Play</button>
+          <button className="ButtonBanner">Add to watch list</button>
+          <p>{movie.overview}</p>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
