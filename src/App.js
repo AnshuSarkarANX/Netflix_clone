4f72e3847afb34d1a4f30dc7cf0854f1724fe314
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-02-10 00:06:44 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import Banner from "./banner";
 function App() {
   return (
     <>
-      <Banner />
       <div className="App">
-        <h1>NETFLIX</h1>
+        <Banner />
         <Row title="Originals" fetchUrl={request.NetflixOriginals} isLargeRow />
         <Row title="Trending Movies" fetchUrl={request.Trending} />
         <Row title="TopRated Movies" fetchUrl={request.TopRated} />
