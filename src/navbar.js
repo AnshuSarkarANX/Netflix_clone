0d7839a6e0f1ccb165c8a583e2e230ffce8fee45
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-02-12 17:00:12 +0530

    new file created for nav bar

---
+++ src/navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./navbar.css";
+
+function navbar() {
+  return (
+    <div className="nav nav_black">
+      <div className="Navbar">
+        <div className="navbar_container">
+          <img
+            className="Logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/800px-Logonetflix.png?20170904093427"
+            alt="logo"
+          />
+          <img
+            className="avatar"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlxIi9VcRXindEC1M94AOqTnCaU0-16LidGg&usqp=CAU"
+            alt="your avatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default navbar;
