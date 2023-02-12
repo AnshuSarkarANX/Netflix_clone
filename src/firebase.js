c0a65d2d7e642c00c5983c432c27dca878b05721
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-02-12 16:59:50 +0530

    new file created for firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwK0I3dVtqydQKOTFa2JEvkCf_J86q11E",
+  authDomain: "netflix-clone-31f2b.firebaseapp.com",
+  projectId: "netflix-clone-31f2b",
+  storageBucket: "netflix-clone-31f2b.appspot.com",
+  messagingSenderId: "1037355892016",
+  appId: "1:1037355892016:web:8f622d4c67c69e3a7f9cf5",
+  measurementId: "G-SST20G1JCW",
+};
