297c1c281aa45b92e900cdbe9194f57e9e0a260d
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-03-07 00:00:37 +0530

    onflick function added on buttons

--- src/Screens/LoginScreen/LoginScreen.js
+++ src/Screens/LoginScreen/LoginScreen.js
@@ -1,9 +1,10 @@
-import React from "react";
+import { React, useState } from "react";
 import "./LoginScreen.css";
+import SignIn from "../SignupScreen/Signup.js";
 
 function LoginScreen() {
+  const [Signin, setSignin] = useState(false);
   return (
-
     <div className="LoginScreen">
       <div className="LoginScreen_background">
         <img
@@ -11,20 +12,33 @@ function LoginScreen() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/800px-Logonetflix.png?20170904093427"
           alt="logo"
         />
-        <button className="SignupButton">Sign Up</button>
+        <button onClick={() => setSignin(true)} className="SignupButton">
+          Sign Up
+        </button>
         <div className="LoginScreen_body">
-          <h1 className="">Unlimited movies, TV shows and more.</h1>
-          <h2 className="">Watch anywhere. Cancel anytime.</h2>
-          <p className="">
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </p>
-          <div className="LoginScreen_input">
-            <form>
-              <input type="email" placeholder="Email address"></input>
-            </form>
-            <button className="LoginScreen_Getstarted">Get Started</button>
-          </div>
+          {SignIn ? (
+            <SignIn />
+          ) : (
+            <>
+              <h1 className="">Unlimited movies, TV shows and more.</h1>
+              <h2 className="">Watch anywhere. Cancel anytime.</h2>
+              <p className="">
+                Ready to watch? Enter your email to create or restart your
+                membership.
+              </p>
+              <div className="LoginScreen_input">
+                <form>
+                  <input type="email" placeholder="Email address"></input>
+                </form>
+                <button
+                  onClick={() => setSignin(true)}
+                  className="LoginScreen_Getstarted"
+                >
+                  Get Started
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
