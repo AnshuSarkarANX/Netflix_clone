b0528d8ad24d485180e9ce6300975698e80bf924
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-04-05 22:52:42 +0530

    `feat` new sign in UI added

--- src/Screens/SignupScreen/Signup.js
+++ src/Screens/SignupScreen/Signup.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import {  createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 import "./Signup.css";
 
 function SignIn() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authuser) => {
+        console.log(authuser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  const signIn = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="SignIn_body">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email address" />
-        <input type="password" placeholder="Password" />
-        <button type="submit" name="submit">
+        <input ref={emailRef} type="email" placeholder="Email address" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" name="submit" onClick={register}>
           Submit
         </button>
         <h4>
           <span className="signUp_grey">New to Netflix? </span>
-          <span className="signUp_link">Signup Now.</span>
+          <span className="signUp_link" onClick={signIn}>
+            Signup Now.
+          </span>
         </h4>
       </form>
     </div>
