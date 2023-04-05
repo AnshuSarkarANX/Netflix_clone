4e818bcecd73a7a1ffa4e8784e7f621d23e2ea58
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-04-05 22:51:54 +0530

    `feat` lines added for authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCwK0I3dVtqydQKOTFa2JEvkCf_J86q11E",
   authDomain: "netflix-clone-31f2b.firebaseapp.com",
@@ -8,3 +11,9 @@ const firebaseConfig = {
   appId: "1:1037355892016:web:8f622d4c67c69e3a7f9cf5",
   measurementId: "G-SST20G1JCW",
 };
+
+const App = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(App);
+
+export { db, auth };
