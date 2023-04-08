d3b1b610f3b21b887cad7521679d3abc9bef5854
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-04-08 17:30:31 +0530

    `feat` frebase authentication added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-// Components
 
+import { onAuthStateChanged } from "firebase/auth";
+//components
 import "./App.css";
 import HomeScreen from "./Screens/HomeScreen/Homescreen.js";
 import LoginScreen from "./Screens/LoginScreen/LoginScreen.js";
+import { auth } from "./firebase.js";
+import { login, logout } from "./features/userSlice";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<HomeScreen />} />)
 );
-const user = 0;
 function App() {
+  const user = null;
+  const diapatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      userAuth
+        ? //logged in
+          diapatch(login({ uid: userAuth.uid, email: userAuth.email }))
+        : //logged out
+          diapatch(logout);
+    });
+    return unsubscribe;
+  });
   return (
     <>
       <div className="App">
