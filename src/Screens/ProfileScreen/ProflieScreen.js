cf0462f63f26794663638929e1cbcc4a15c755db
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-04-28 00:12:11 +0530

    firebase file added

--- src/Screens/ProfileScreen/ProflieScreen.js
+++ src/Screens/ProfileScreen/ProflieScreen.js
@@ -3,6 +3,7 @@ import "./ProflieScreen.css";
 import Navbar from "../../Screens/HomeScreen/navbar.js";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
+import { auth } from "../../firebase";
 
 function ProflieScreen() {
   const user = useSelector(selectUser);
@@ -11,15 +12,26 @@ function ProflieScreen() {
       <Navbar />
       <div className="body">
         <h1>Edit Profile</h1>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlxIi9VcRXindEC1M94AOqTnCaU0-16LidGg&usqp=CAU"
-          alt="your avatar"
-        />
-        <div className="Details">
-          <h2>{user.email}</h2>
-          <div className="plans"></div>
+        <div className="Info">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlxIi9VcRXindEC1M94AOqTnCaU0-16LidGg&usqp=CAU"
+            alt="your avatar"
+          />
+          <div className="Details">
+            <h2>{user.email}</h2>
+            <div className="Plans">
+              <h3>Plans</h3>
+              <button
+                className="SignOut"
+                onClick={() => {
+                  auth.signOut();
+                }}
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
         </div>
-        <button className="signout">Sign Out </button>
       </div>
     </div>
   );
