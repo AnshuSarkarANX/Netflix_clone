0c9feac476e9ab4a621eac938fd4b3e5cd62f13b
Anshu Sarkar <anshusarkaranx@gmail.com>
2023-08-04 23:58:56 +0530

    updated

--- src/Screens/LoginScreen/LoginScreen.js
+++ src/Screens/LoginScreen/LoginScreen.js
@@ -1,12 +1,11 @@
 import { React, useState } from "react";
 import { auth } from "../../firebase";
 import { signInAnonymously } from "firebase/auth";
+import SignIn from "../SignupSignin/Signin";
 import "./LoginScreen.css";
-import SignIn from "../SignupScreen/Signup.js";
 
 function LoginScreen() {
-  const [Signin, setSignin] = useState(false);
-
+  const [signIn, setsignIn] = useState(false);
   return (
     <div className="LoginScreen">
       <div className="LoginScreen_background">
@@ -15,40 +14,39 @@ function LoginScreen() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/800px-Logonetflix.png?20170904093427"
           alt="logo"
         />
-        <button onClick={() => setSignin(true)} className="SignupButton">
+        <button onClick={() => setsignIn(true)} className="SignupButton">
           Sign Up
         </button>
         <div className="LoginScreen_body">
-          {Signin ? (
-            <SignIn />
-          ) : (
+         { signIn  ?(
+          <SignIn />) :(
             <>
-              <h1 className="">Unlimited movies, TV shows and more.</h1>
-              <h2 className="">Watch anywhere. Cancel anytime.</h2>
-              <p className="">
-                Ready to watch? Enter your email to create or restart your
-                membership.
-              </p>
-              <div className="LoginScreen_input">
-                <form>
-                  <input type="email" placeholder="Email address"></input>
-                </form>
-                <button
-                  onClick={() => setSignin(true)}
-                  className="LoginScreen_Getstarted"
-                >
-                  Get Started
-                </button>
-              </div>
-              <div className="skipButton">
-                <button
-                  className="LoginScreen_Skip"
-                  onClick={() => signInAnonymously(auth)}
-                >
-                  Skip To Homepage
-                </button>
-              </div>
-            </>
+          <h1 className="">Unlimited movies, TV shows and more.</h1>
+          <h2 className="">Watch anywhere. Cancel anytime.</h2>
+          <p className="">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+          <div className="LoginScreen_input">
+            <form>
+              <input type="email" placeholder="Email address"></input>
+            </form>
+            <button
+              onClick={() => setsignIn(true)}
+              className="LoginScreen_Getstarted"
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="skipButton">
+            <button
+              className="LoginScreen_Skip"
+              onClick={() => signInAnonymously(auth)}
+            >
+              Skip To Homepage
+            </button>
+          </div>
+          </>
           )}
         </div>
       </div>
